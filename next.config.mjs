cd2999a408e3086aa45c5db89625c9c774cceee3
ShoheiAsogawa <uken.shohei@gmail.com>
2026-05-16 17:49:40 +0900

    Fix next.config.mjs with correct assetPrefix for image paths

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,15 @@ const isProd = process.env.NODE_ENV === 'production';
 const basePath = isProd ? '/FirstCourage-HP' : '';
 
 const nextConfig = {
-      output: 'export',
-      basePath,
-      assetPrefix: basePath,
-      env: {
-              NEXT_PUBLIC_BASE_PATH: basePath,
-      },
-      images: {
-              unoptimized: true,
-      },
+  output: 'export',
+  basePath,
+  assetPrefix: basePath,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
